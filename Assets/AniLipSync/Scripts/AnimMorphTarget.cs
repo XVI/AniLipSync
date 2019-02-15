4427ed2e9402bd537418d3b919a0d0271ecdf58d
Hirotoshi Yoshitaka <yoshitaka@xvi.co.jp>
2019-02-15 17:10:21 +0900

    インスペクタにデバッグ用のプレビューを追加

--- Assets/AniLipSync/Scripts/AnimMorphTarget.cs
+++ Assets/AniLipSync/Scripts/AnimMorphTarget.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace XVI.AniLipSync {
     public class AnimMorphTarget : MonoBehaviour {
@@ -108,4 +111,85 @@ namespace XVI.AniLipSync {
             skinnedMeshRenderer.SetBlendShapeWeight(visemeToBlendShape[visemeIndex], transitionCurves[visemeIndex].Evaluate(transitionTimer) * curveAmplifier);
         }
     }
+
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(AnimMorphTarget))]
+    public class AnimMorphTargetEditor : Editor {
+        static readonly string[] visemeNames = new string[] { "sil(無音)", "PP(子音)", "FF(子音)", "TH(子音)", "DD(子音)", "kk(子音)", "CH(子音)", "SS(子音)", "nn(ん)", "RR(子音)", "aa(あ)", "E(え)", "ih(い)", "oh(お)", "ou(う)" };
+
+        OVRLipSyncContextBase lipSyncContext;
+
+        public override bool HasPreviewGUI() {
+            return true;
+        }
+
+        public override bool RequiresConstantRepaint() {
+            return false;
+        }
+
+        public override GUIContent GetPreviewTitle() {
+            return new GUIContent("AnimMorphTarget Debug Preview");
+        }
+
+        void OnEnable() {
+            var targetComponent = target as AnimMorphTarget;
+            lipSyncContext = targetComponent.GetComponent<OVRLipSyncContextBase>();
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background) {
+            base.OnPreviewGUI(r, background);
+
+            var animMorphTargetType = (target as AnimMorphTarget).GetType();
+            var field = animMorphTargetType.GetField("previousViseme", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var previousViseme = (int)(OVRLipSync.Viseme)field.GetValue(target);
+
+            var frame = lipSyncContext.GetCurrentPhonemeFrame();
+            var visemeIndex = 0;
+
+            var whiteStyle = new GUIStyle();
+            whiteStyle.normal.textColor = Color.white;
+            var redStyle = new GUIStyle();
+            redStyle.normal.textColor = Color.magenta;
+
+            foreach (var visemeName in visemeNames) {
+                var labelRect = new Rect(r) {
+                    width = 100,
+                    height = EditorGUIUtility.singleLineHeight
+                };
+                r.y += labelRect.height;
+
+                var style = whiteStyle;
+                if (visemeIndex == previousViseme) {
+                    style = redStyle;
+                }
+                GUI.Label(labelRect, visemeName, style);
+
+                var weight = frame.Visemes[visemeIndex];
+                var weightRect = new Rect(labelRect) {
+                    x = labelRect.x + labelRect.width
+                };
+                GUI.Label(weightRect, weight.ToString("0.000"), whiteStyle);
+                visemeIndex++;
+            }
+            r.y += 10;
+
+            var rmsProvider = (target as AnimMorphTarget).GetComponent<LowLatencyLipSyncContext>();
+            if (rmsProvider != null) {
+                var rmsLabelRect = new Rect(r) {
+                    width = 100,
+                    height = EditorGUIUtility.singleLineHeight
+                };
+                GUI.Label(rmsLabelRect, "RMS", whiteStyle);
+                var rms = rmsProvider.GetMicVolume();
+                var rmsRect = new Rect(r) {
+                    x = rmsLabelRect.x + rmsLabelRect.width,
+                    width = 100,
+                    height = EditorGUIUtility.singleLineHeight
+                };
+                GUI.Label(rmsRect, rms.ToString("0.00000"), whiteStyle);
+                r.y += rmsLabelRect.height;
+            }
+        }
+    }
+    #endif
 }
